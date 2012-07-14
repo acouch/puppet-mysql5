89035fc3ad7c722cb3605154700c5fb38d61799f
acouch <acinternets@gmail.com>
2012-07-13 22:26:43 -0400

    updating init.pp to separate mysql packages which inexplicably keeps mysql init script from failing

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,31 @@
-class mysql5($mysqlpassword, $webadminuser = "root", $webadmingroup = "root") {
-  package { "mysql":
-      name => [
-        "mysql-server",
-        "mysql-client",
-        "mysql-common",
-      ],
-      ensure => installed,
-  }
+class mysql5(
+  $mysqlpassword,
+  $webadminuser = "root",
+  $webadmingroup = "root") {
 
-  service { "mysql":
-    ensure => "running",
-  }
+
+   package { 'mysql-server':
+     ensure => installed,
+   }
+
+   package { 'mysql-client':
+     ensure => installed,
+   }
+
+   package { 'mysql-common':
+     ensure => installed,
+   }
+
+   # Recommended here: http://projects.puppetlabs.com/issues/5610#note-13
+   # /etc/init.d/mysql status fails without dbus installed but adding dbus as a package did
+   # not fix that on first provision. Not sure why this works but it does.
+   service { "mysql":
+     alias => "mysql-server",
+     ensure => running,
+     enable => true,
+     require => Package["mysql-server"],
+     hasstatus => true,
+   }
 
   # TODO: This only does the initial set, it won't reset it.
   exec { "Set MySQL server root password":
@@ -24,7 +39,7 @@ class mysql5($mysqlpassword, $webadminuser = "root", $webadmingroup = "root") {
     unless => "mysqladmin -uroot -p$mysqlpassword status",
     path => ["/bin", "/usr/bin"],
     command => "mysqladmin -uroot password $mysqlpassword",
-    require => Package["mysql"],
+    require => Package["mysql-server"],
   }
 
   file { 'my.cnf':
